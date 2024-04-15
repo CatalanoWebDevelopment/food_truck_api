8624a67282ed45c538b2418f2f5f73e0189eed1f
Parker Catalano <catalano.work@gmail.com>
2024-04-14 21:12:15 -0400

    feat: Add config/docs.exs

---
+++ config/docs.exs
@@ -0,0 +1,29 @@
+use Mix.Config
+
+config :ex_doc, :elixir_version, System.fetch_env!("ELIXIR_VERSION", "1.12.3")
+config :ex_doc, :output_dir, "priv/static/docs"
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint,
+  docs: [
+    source_paths: ["lib", "test"],
+    exclude: ~r/_build|deps/
+  ]
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint,
+  url: [scheme: "https", host: System.get_env("APP_NAME") <> ".herokuapp.com", port: 80],
+  http: [port: 80],
+  cache_static_manifest: "priv/static/cache_manifest.json"
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint, load_from_system_env: true
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint, server: true
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint,
+  root: ".",
+  version: Mix.Project.config()[:version],
+  name: "FoodTruckApi"
+
+config :food_truck_api, FoodTruckApiWeb.Endpoint, compilers: [:gettext] ++ Mix.compilers()
